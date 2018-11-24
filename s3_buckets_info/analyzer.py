15d87c3aa8897f59c3bf2c8425244f253a763c77
David Castellanos <davidcaste@gmail.com>
2018-11-24 01:42:47 +0100

    MVP

---
+++ s3_buckets_info/analyzer.py
@@ -0,0 +1,42 @@
+from __future__ import (absolute_import, division, print_function, unicode_literals)
+__metaclass__ = type
+
+import boto3
+
+
+def analyze_buckets():
+    client = boto3.client('s3')
+    response = client.list_buckets()
+
+    buckets_info = []
+
+    for bucket in response['Buckets']:
+        objects_num, objects_size = _get_bucket_info(bucket['Name'])
+
+        bucket_info = {
+            'name': bucket['Name'],
+            'creation_date': bucket['CreationDate'],
+            'objects_num': objects_num,
+            'objects_size': objects_size
+        }
+
+        buckets_info.append(bucket_info)
+
+    return buckets_info
+
+
+def _get_bucket_info(name):
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(name)
+
+    objects_num = 0
+    objects_size = 0
+
+    for obj in bucket.objects.all():
+        objects_num += 1
+        objects_size += obj.size
+
+    return objects_num, objects_size
+
+
+
--- s3_buckets_info/main.py
+++ s3_buckets_info/main.py
@@ -3,6 +3,7 @@
 s3-buckets-info analyzes S3 buckets
 
 Usage:
+  s3-buckets-info
   s3-buckets-info -h | --help
   s3-buckets-info -v | --version
 
@@ -17,6 +18,7 @@ __metaclass__ = type
 import docopt
 
 from s3_buckets_info import __version__
+from s3_buckets_info.analyzer import analyze_buckets
 
 
 def main():
@@ -25,6 +27,13 @@ def main():
     if args['--version']:
         print('s3-buckets-info {}'.format(__version__))
         return
+
+    for bucket_info in analyze_buckets():
+        print('''
+Bucket "{name}":
+  Created at: {creation_date}
+  Number of files: {objects_num}
+  Total size of files: {objects_size}'''.format(**bucket_info))
 
 
 if __name__ == '__main__':
